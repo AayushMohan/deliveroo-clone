7eb58bc2f35644e70a31f8c3b9583d1207b071e0
AayushMohan <aayushmohan1702@gmail.com>
2022-07-30 10:52:19 +0530

    Added: User Icon

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -18,21 +18,23 @@ const HomeScreen = () => {
   }, []);
 
   return (
-    <SafeAreaView>
+    <SafeAreaView className="bg-white pt-5">
       <Text className="text-red-500">
         {/* Header */}
-        <View className="flex-row pb-3 items-center mx-4 space-x-2">
+        <View className="flex-row pb-3 items-center mx-4 space-x-2 px-2">
           <Image
             source={{ uri: "https://links.papareact.com/wru" }}
             className="h-7 w-7 bg-gray-300 p-4 rounded-full"
           />
-          <View>
+          <View className="flex-1">
             <Text className="font-bold text-gray-400 text-xs">Deliver Now</Text>
             <Text className="font-bold text-xl">
               Current Location
               <ChevronDownIcon size={20} color="#00CCBB" />
             </Text>
           </View>
+
+          <UserIcon size={35} color="#00CCBB" />
         </View>
       </Text>
     </SafeAreaView>
