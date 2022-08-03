8612d67ccc5e4eb352797fc00e59e9b508008efc
AayushMohan <aayushmohan1702@gmail.com>
2022-08-03 23:39:11 +0530

    Added: Location Marker

--- screens/RestaurantScreen.js
+++ screens/RestaurantScreen.js
@@ -3,6 +3,12 @@ import React, { useLayoutEffect } from "react";
 import { useNavigation, useRoute } from "@react-navigation/native";
 import { urlFor } from "../sanity";
 import { ArrowLeftIcon } from "react-native-heroicons/solid";
+import {
+  ChevronRightIcon,
+  LocationMarkerIcon,
+  StarIcon,
+} from "react-native-heroicons/solid";
+import { QuestionMarkCircleIcon } from "react-native-heroicons/outline";
 
 const RestaurantScreen = () => {
   const navigation = useNavigation();
@@ -45,7 +51,21 @@ const RestaurantScreen = () => {
       <View className="bg-white">
         <View className="px-4 pt-4">
           <Text className="text-3xl font-bold">{title}</Text>
-          <View className="flex-row space-x-2 my-1"></View>
+          <View className="flex-row space-x-2 my-1">
+            <View className="flex-row items-center space-x-1">
+              <StarIcon color="green" opacity={0.5} size={22} />
+              <Text className="text-xs text-gray-500">
+                <Text className="text-green-500">{rating}</Text> . {genre}
+              </Text>
+            </View>
+
+            <View className="flex-row items-center space-x-1">
+              <LocationMarkerIcon color="green" opacity={0.4} size={22} />
+              <Text className="text-xs text-gray-500">
+                <Text className="text-green-500">{rating}</Text> . {genre}
+              </Text>
+            </View>
+          </View>
         </View>
       </View>
     </ScrollView>
