9980fce4c624c58ca316f5ea0ae83a889ceb3f99
AayushMohan <aayushmohan1702@gmail.com>
2022-08-05 02:58:45 +0530

    Added: Basket Items

--- components/BasketIcon.js
+++ components/BasketIcon.js
@@ -1,8 +1,9 @@
-import { View, Text } from "react-native";
+import { View, Text, TouchableOpacity } from "react-native";
 import React from "react";
 import { useNavigation } from "@react-navigation/native";
 import { useSelector } from "react-redux";
-import { selectBasketTotal } from "../features/basketSlice";
+import { selectBasketItems, selectBasketTotal } from "../features/basketSlice";
+import Currency from "react-currency-formatter";
 
 const BasketIcon = () => {
   const items = useSelector(selectBasketItems);
@@ -10,8 +11,18 @@ const BasketIcon = () => {
   const basketTotal = useSelector(selectBasketTotal);
 
   return (
-    <View>
-      <Text></Text>
+    <View className="absolute bottom-10 w-full z-50">
+      <TouchableOpacity className="bg-[#00CCBB] mx-5 p-4 rounded-lg flex-row items-center space-x-1">
+        <Text className="text-white font-extrabold text-lg bg-[#01A296] py-1 px-2">
+          {items.length}
+        </Text>
+        <Text className="text-white flex-1 font-extrabold text-lg text-center">
+          View Basket
+        </Text>
+        <Text className="text-lg text-white font-extrabold">
+          <Currency quantity={basketTotal} currency="INR" />
+        </Text>
+      </TouchableOpacity>
     </View>
   );
 };
