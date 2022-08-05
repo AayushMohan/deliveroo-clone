33196bbf40d85796948504700cd68b532210a85c
AayushMohan <aayushmohan1702@gmail.com>
2022-08-05 17:01:47 +0530

    Added: PreparingOrderScreen

--- screens/BasketScreen.js
+++ screens/BasketScreen.js
@@ -118,7 +118,10 @@ const BasketScreen = () => {
             </Text>
           </View>
 
-          <TouchableOpacity className="rounded-lg p-4 bg-[#00CCBB]">
+          <TouchableOpacity
+            onPress={() => navigation.navigate("PreparingOrderScreen")}
+            className="rounded-lg p-4 bg-[#00CCBB]"
+          >
             <Text className="text-center text-white text-lg font-bold">
               Place Order
             </Text>
