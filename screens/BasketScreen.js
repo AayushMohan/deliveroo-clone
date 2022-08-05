7ad9ee08c02867f59e938a93a65d4a64ea8bb686
AayushMohan <aayushmohan1702@gmail.com>
2022-08-05 15:07:52 +0530

    Added: restaurantSlice and View Basket Menu

--- screens/BasketScreen.js
+++ screens/BasketScreen.js
@@ -1,11 +1,44 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { View, Text, SafeAreaView, TouchableOpacity } from "react-native";
+import React, { useEffect, useState } from "react";
+import { useNavigation } from "@react-navigation/native";
+import { selectRestaurant } from "../features/restaurantSlice";
+import { selectBasketItems } from "../features/basketSlice";
+import { useSelector, useDispatch } from "react-redux";
+import { useMemo } from "react";
+import { XCircleIcon } from "react-native-heroicons/solid";
 
 const BasketScreen = () => {
+  const navigation = useNavigation();
+  const restaurant = useSelector(selectRestaurant);
+  const items = useSelector(selectBasketItems);
+  const [groupedItemsInBasket, setGroupedItemsInBasket] = useState([]);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const groupedItems = items.reduce((results, item) => {
+      (results[item.id] = results[item.id] || []).push(item);
+      return results;
+    }, {});
+
+    setGroupedItemsInBasket(groupedItems);
+  }, [items]);
+
   return (
-    <View>
-      <Text>BasketScreen</Text>
-    </View>
+    <SafeAreaView className="flex-1 bg-white">
+      <View className="flex-1 bg-gray-100">
+        <View className="p-5 border-b border-[#00CCBB] shadow-xs">
+          <Text className="text-lg font-bold text-center">Basket</Text>
+          <Text className="text-center text-gray-400">{restaurant.title}</Text>
+        </View>
+
+        <TouchableOpacity
+          onPress={navigation.goBack}
+          className="rounded-full bg-gray-100 absolute top-3 right-5"
+        >
+          <XCircleIcon color="#00CCBB" height={50} width={50} />
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
   );
 };
 
