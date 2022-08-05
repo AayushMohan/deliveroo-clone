3833d62a34c3127584f307f6cfb888e09f7eefa1
AayushMohan <aayushmohan1702@gmail.com>
2022-08-05 15:18:20 +0530

    Added BasketScreen Details

--- screens/BasketScreen.js
+++ screens/BasketScreen.js
@@ -1,4 +1,10 @@
-import { View, Text, SafeAreaView, TouchableOpacity } from "react-native";
+import {
+  View,
+  Text,
+  SafeAreaView,
+  TouchableOpacity,
+  Image,
+} from "react-native";
 import React, { useEffect, useState } from "react";
 import { useNavigation } from "@react-navigation/native";
 import { selectRestaurant } from "../features/restaurantSlice";
@@ -26,17 +32,32 @@ const BasketScreen = () => {
   return (
     <SafeAreaView className="flex-1 bg-white">
       <View className="flex-1 bg-gray-100">
-        <View className="p-5 border-b border-[#00CCBB] shadow-xs">
-          <Text className="text-lg font-bold text-center">Basket</Text>
-          <Text className="text-center text-gray-400">{restaurant.title}</Text>
+        <View className="p-5 border-b border-[#00CCBB] bg-white shadow-xs">
+          <View>
+            <Text className="text-lg font-bold text-center">Basket</Text>
+            <Text className="text-center text-gray-400">
+              {restaurant.title}
+            </Text>
+          </View>
+
+          <TouchableOpacity
+            onPress={navigation.goBack}
+            className="rounded-full bg-gray-100 absolute top-3 right-5"
+          >
+            <XCircleIcon color="#00CCBB" height={50} width={50} />
+          </TouchableOpacity>
         </View>
 
-        <TouchableOpacity
-          onPress={navigation.goBack}
-          className="rounded-full bg-gray-100 absolute top-3 right-5"
-        >
-          <XCircleIcon color="#00CCBB" height={50} width={50} />
-        </TouchableOpacity>
+        <View className="flex-row items-center space-x-4 px-4 py-3 bg-white my-5">
+          <Image
+            source={{ uri: "https://links.papareact.com/wru" }}
+            className="h-7 w-7 bg-gray-300 p-4 rounded-full"
+          />
+          <Text className="flex-1">Deliver in 50-75 min</Text>
+          <TouchableOpacity>
+            <Text className="text-[#00CCBB]">Change</Text>
+          </TouchableOpacity>
+        </View>
       </View>
     </SafeAreaView>
   );
